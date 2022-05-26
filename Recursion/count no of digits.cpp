13661a8f9b087e7af975d4e9b21f0846c807e5a8
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-05-26 10:07:57 +0530

    no of digit

---
+++ Recursion/count no of digits.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+  
+    cout << count(n) << endl;
+}
+
+int count(int n){
+    
+    if(n==0){
+        return 0;
+    }
+    
+    int so = count(n/10);
+    return 1 + so;
+}
+
+
