20d97334ba3b3279920e642f0af12b3dda508ffd
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-06-07 09:32:50 +0530

    Power

---
+++ Recursion/Pow.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include "Solution.h"
+using namespace std;
+
+int power(int x, int n) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    
+    if(n==0){
+        return 1;
+    }
+    
+    int smallOutput = x*power(x, n-1);
+    return smallOutput;
+
+}
+
+int main(){
+    int x, n;
+    cin >> x >> n;
+  
+    cout << power(x, n) << endl;
+}
+
+
