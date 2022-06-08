9f47a2a5c8bb5f529eb46f403c3e8358f2301822
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-06-08 19:20:56 +0530

    first occ without helper fn

--- Recursion/First Index of num in array.cpp
+++ Recursion/First Index of num in array.cpp
@@ -15,7 +15,22 @@ int helper(int input[], int size, int x, int i){
 
 
 int firstIndex(int input[], int size, int x) {
-	return helper(input, size, x, 0);
+    // return helper(input, size, x, 0);
+    if(size == 0){
+        return -1;
+    }
+    
+    if(input[0] == x){
+        return 0;
+    }
+    
+    int ans = firstIndex(input+1, size-1, x);
+    
+    if(ans == -1){
+        return -1;
+    } else {
+        return ans + 1;
+    }
 }
 
 int main(){
