6b053113a6305d92954bd1d2b316850ad473413a
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-06-08 14:11:21 +0530

    find if element in array

--- Recursion/sum of array.cpp
+++ Recursion/sum of array.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 int sum(int a[], int n) {
 	if(n==1){
         return a[0];
@@ -5,4 +8,17 @@ int sum(int a[], int n) {
     
 	int smallArray = sum(a+1, n-1);
     return a[0] + smallArray;
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    cout << sum(input, n) << endl;
 }
