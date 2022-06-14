5097b122c3b73b4c9884df060864dd677d7bfa9a
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-06-14 08:25:55 +0530

    desc added

--- Recursion/all keypad combinations.cpp
+++ Recursion/all keypad combinations.cpp
@@ -1,3 +1,24 @@
+/*
+Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
+Return empty string for numbers 0 and 1.
+Note : 1. The order of strings are not important.
+2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
+
+Sample Input:
+23
+
+Sample Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf
+*/
+
 #include <iostream>
 #include <cstring>
 #include <vector>
