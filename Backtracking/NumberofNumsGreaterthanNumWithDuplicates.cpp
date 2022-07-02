2665f076e5854539b629862d71f699184f57301c
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-07-02 15:39:25 +0530

    find number of nums greater than num which has repeating digits as well

--- Backtracking/NumberofNumsGreaterthanNumWithDuplicates.cpp
+++ Backtracking/NumberofNumsGreaterthanNumWithDuplicates.cpp
@@ -1,3 +1,17 @@
+/*
+Code : No. of numbers - with duplicates
+Send Feedback
+Given a number n, find number of numbers that are greater than n using the same set of digits as n. n might have duplicate digits.
+Input Format :
+Integer n
+Output Format :
+Count of greater numbers
+Sample Input :
+122
+Sample Output :
+2
+*/
+
 #include <iostream>
 #include <cstring>
 #include <vector>
