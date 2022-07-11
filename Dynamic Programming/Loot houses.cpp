3e1c7c2a4926b479876da43bfaf729fbcaa4d127
freakfan15 <vivekkrrai.bxr@gmail.com>
2022-07-11 18:54:45 +0530

    m 2

--- Dynamic Programming/Loot houses.cpp
+++ Dynamic Programming/Loot houses.cpp
@@ -62,6 +62,20 @@ int maxMoneyLooted(int *arr, int n)
     return ans;
 }
 
+//using iteration
+int maxMoneyLooted2(int * arr, int n){
+	int* dp = new int[n];
+
+	dp[0] = arr[0];
+	dp[1] = arr[1];
+
+	for(int i=2; i<n; i++){
+		dp[i] = max(dp[i-2] + arr[i], dp[i-1]);
+	}
+
+	return dp[n-1];
+}
+
 int main() {
 
 	#ifndef ONLINE_JUDGE
